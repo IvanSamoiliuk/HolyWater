2d9b7f522e3a285c3419405db396584f36f646c3
Ivan Samoiliuk <bessob10@gmail.com>
2024-04-04 15:42:31 +0300

    Added constants Colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  white: '#FFF',
+  black: 'black',
+  lightgrey: '#FFFFFF',
+  darkgrey: '#A9A9A9',
+  blue: '#00BFE5',
+  darkblue: '#3598D0',
+  shadowText: 'rgba(0, 0, 0, 0.25)'
+};
